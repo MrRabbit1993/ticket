0d395ef854b0debd04f186ed3ba9e5748d2c49d2
兔子先森 <1993iweb@gmail.com>
2020-03-10 14:35:52 +0800

    eject后配置module.less

---
+++ src/components/b/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./index.module.less"
+function B(peops) {
+    return <div className={styles.color}>456
+        <div className={styles.color}>789</div>
+    </div>
+}
+export default B;
